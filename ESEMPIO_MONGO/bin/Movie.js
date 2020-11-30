b90ab238429e0918da0173567cdede7c7a3d3b35
capuzzo-susanna <capuzzo.susanna@iisgalvanimi.edu.it>
2020-11-30 11:51:09 +0000

    preparazione non completa

--- ESEMPIO_MONGO/bin/Movie.js
+++ ESEMPIO_MONGO/bin/Movie.js
@@ -20,4 +20,23 @@ router.get('/', function (req, res, next) {
     
 });
 
+
+router.get('/movie_from_title/:title', function (req, res, next) {
+    console.log(req.params); //Leggo i parametri passati all'url
+    title = req.params.title;
+    const uri = "mongodb+srv://Scapuzzo:Galvani2020@nyc-1.3v9pr.mongodb.net/NYC-1?retryWrites=true&w=majority"
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
+        // eseguo una find sulla collection
+        collection.find({ 'title': `${title}` }).toArray((err, result) => {
+            if (err) console.log(err.message); //Se c'è qualche errore lo stampo
+            else res.send(result);
+            client.close(); //Quando ho terminato la find chiudo la sessione con il db
+        }); //Eseguo la query e passo una funzione di callback
+
+    });
+});
+
 module.exports = router;
+
